787e7a3b814493c2a3cd1ecb9d97cf986856a235
Fctvzm <zaytkalieva739@gmail.com>
2018-10-03 17:59:59 +0600

    add fetch

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,55 +1,39 @@
 import React, { Component } from 'react';
 import {Image} from 'react-bootstrap'
 import './css/Recipes.css'
-
-import cat_1 from "./css/images/cat_1.jpg"
-import cat_2 from "./css/images/cat_2.jpg"
-import cat_3 from "./css/images/cat_3.jpg"
-import cat_4 from "./css/images/cat_4.jpg"
-import cat_5 from "./css/images/cat_5.jpg"
-
-
-var CategoriesArray = [
-	{
-		id: 1,
-		name: 'Dessert',
-		image: cat_3
-	},
-	{
-		id: 2,
-		name: 'Meat',
-		image: cat_5
-	},
-	{
-		id: 3,
-		name: 'Vegetarian',
-		image: cat_4
-	},
-	{
-		id: 4,
-		name: 'Sea food',
-		image: cat_2
-	},
-	{
-		id: 5,
-		name: 'Soup',
-		image: cat_1
-	},
-];
+const SERVER_BASE_URL = "http://localhost:8000";
+const BASE_URL = 'http://localhost:8000/categories/';
 
 class Categories extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			categories: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(`${BASE_URL}`)
+		.then(data => data.json())
+		.then((data) => {
+			this.setState({
+				categories: data
+			})
+		})
+		console.log(this.state.recipes);
+	}
 
 	createCategoryRow(category) {
 		return <div key = {category.id} 
 					className = "Recipes-item"
 					onClick = {() => this.props.history.push(`/Recipes/${category.id}`)}>
-					<Image src={category.image} responsive rounded/>
+					<Image src={SERVER_BASE_URL + category.image} responsive rounded/>
 					<p className="category-label">{category.name}</p>
 				</div>
 	}
 
 	render() {
-		var categories = CategoriesArray.map(this.createCategoryRow.bind(this));
+		var categories = this.state.categories.map(this.createCategoryRow.bind(this));
 		return (
 		 	<div className="Recipes-container">
 	     		<h2>Categories</h2>
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,81 +2,33 @@ import React, { Component } from 'react';
 import {Image} from 'react-bootstrap';
 import "./css/Recipes.css";
 
-import special_image from "./css/images/Special_image.jpg";
-import LRB_1 from "./css/images/LRB_1.jpg";
-import SR_1 from "./css/images/SR_1.jpg";
-import RP_1 from "./css/images/RP_1.jpg";
-import RP_2 from "./css/images/RP_2.jpg";
-import RP_3 from "./css/images/RP_3.jpg";
-import RP_4 from "./css/images/RP_4.jpg";
-import RP_5 from "./css/images/RP_5.jpg";
 import sorry_image from "./css/images/sorry_image.svg";
-
-
-var RecipesArray = [
-	{
-		id:0,
-		image: special_image,
-		name: 'Pulled BBQ jackfruit sandwich',
-		category: 3
-	},
-	{
-		id:1,
-		image: LRB_1,
-		name: 'Mapo tofu',
-		category: 3
-	},
-	{
-		id:2,
-		image: SR_1,
-		name: 'Sheet pan sesame chicken and vegetables',
-		category: 2
-	},
-	{
-		id:3,
-		image: RP_1,
-		name: 'Warming sweet potato and bean chili',
-		category: 3
-	},
-	{
-		id:4,
-		image: RP_2,
-		name: 'Savoy cabbage quiche',
-		category: 4
-	},
-	{
-		id:5,
-		image: RP_3,
-		name: 'Shrimp and sausage gumbo',
-		category: 5
-	},
-	{
-		id:6,
-		image: RP_4,
-		name: 'Paleo chocolate muffins',
-		category: 1
-	},
-	{
-		id:7,
-		image: RP_5,
-		name: 'Vegetable stir-fry with ginger-lime sauce',
-		category: 3
-	}
-];
-
-var count = 0;
-const size = RecipesArray.length;
+const SERVER_BASE_URL = "http://localhost:8000";
+const BASE_URL = 'http://localhost:8000/recipes/';
 
 class Recipes extends Component {
 	constructor(props) {
 		super(props);
-		count = 0;
+		this.state = {
+			recipes: []
+		}
 	}
+
+	componentDidMount() {
+		fetch(`${BASE_URL}`)
+		.then(data => data.json())
+		.then((data) => {
+			this.setState({
+				recipes: data
+			})
+		})
+		console.log(this.state.recipes);
+	}
+
 
 	createRecipeRow(recipe) {
 		var filter = "";
 		var category = 0;
-		console.log(category);
 
 		if (typeof this.props.filter !== 'undefined') 
 			filter = this.props.filter;
@@ -84,9 +36,8 @@ class Recipes extends Component {
 		if(typeof this.props.match !== 'undefined')
 			category = this.props.match.params.id;
 
-		var name = recipe.name.toLowerCase();
+		var name = recipe.title.toLowerCase();
 		if (name.indexOf(filter) === -1) {
-			count++;
 			return;
 		}
 
@@ -97,32 +48,33 @@ class Recipes extends Component {
 		return <div key = {recipe.id} 
 					className = "Recipes-item"
 					onClick = {() => this.props.history.push(`/RecipeInfo/${recipe.id}`)}>
-					<Image src={recipe.image} responsive rounded/>
-					<p>{recipe.name}</p>
+					<Image src={SERVER_BASE_URL + recipe.image} responsive rounded/>
+					<p>{recipe.title}</p>
 				</div>
 	}
 
 
   	render() {
-  		var recipes = RecipesArray.map(this.createRecipeRow.bind(this));
-  		if (count === size) {
-  			count = 0;
+  		var recipes = this.state.recipes.map(this.createRecipeRow.bind(this));
+  		console.log(recipes.length);
+  		if (recipes.length === 0 && this.state.recipes.length !== 0) {
   			return (
   				<div className="Recipes-container">
   					<Image src = {sorry_image}/>
   				</div>
   			)
   		}
-	    count = 0;
-	    return (
-	     	<div className="Recipes-container">
-	     		<h2>Recipes</h2>
-	     		<div className="Recipes-row">
-	     			{recipes}
-	     		</div>
-	     	</div>
-	    );
-  }
+  		else {
+		    return (
+		     	<div className="Recipes-container">
+		     		<h2>Recipes</h2>
+		     		<div className="Recipes-row">
+		     			{recipes}
+		     		</div>
+		     	</div>
+		    );
+		}
+  	}
 }
 
 export default Recipes;
